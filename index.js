d620b0a03f72204f0280711e865d2d44472f34f1
Will Plumb <zrenicaplumb@gmail.com>
2020-08-09 20:28:41 -0600

    .

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //defines the gameArea object
+let gamePiece;
+
 const gameArea = {
      //defines the gameArea.canvas property
      canvas: document.createElement("canvas"),
@@ -7,6 +9,7 @@ const gameArea = {
           this.setupCanvasDimensions();
           this.setContext();
           this.appendToHtml();
+          this.interval();
      },
      setupCanvasDimensions() {
           this.canvas.width = 900;
@@ -19,12 +22,40 @@ const gameArea = {
      appendToHtml() {
           document.body.insertBefore(this.canvas, document.body.childNodes[0]);
      },
+     interval() {
+          setInterval(updateGameArea, 20);
+     },
+     clear() {
+          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+     },
 };
+
+function Component(width, height, color, x, y) {
+     this.width = width;
+     this.height = height;
+     this.color = color;
+     this.x = x;
+     this.y = y;
+     //update the component
+     this.update = () => {
+          let context = gameArea.context;
+          context.fillStyle = color;
+          context.fillRect(this.x, this.y, this.width, this.height);
+     };
+}
+
+function updateGameArea() {
+     gameArea.clear();
+     gamePiece.x += 1;
+     gamePiece.update();
+}
 
 //defines the startGame function
 const startGame = () => {
      //calls gameArea object's setup method
      gameArea.setup();
+     //instantiate gamePiece
+     gamePiece = new Component(100, 100, "red", 250, 250);
 };
 //calls the startGame function
 
